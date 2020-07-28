06126ecb906acdeddff8aa1f5712c0309859a111
Caio Torres <caiocftorres@gmail.com>
2020-07-27 22:56:21 -0300

    First commit

---
+++ src/test/java/com/yulken/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulken.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
