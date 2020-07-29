bb0b7e33a8080b231974c2087851cbd5f241810b
Caio Torres <caiocftorres@gmail.com>
2020-07-28 22:36:54 -0300

    Filtering users by id

--- src/main/java/com/yulken/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/yulken/workshopmongo/dto/UserDTO.java
@@ -7,14 +7,23 @@ import com.yulken.workshopmongo.domain.User;
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L; 
 	
+	private String id;
 	private String name;
 	private String email;
 	
 	public UserDTO() {	
 	}
 	public UserDTO(User obj) {
+		id = obj.getId();
 		name = obj.getName();
 		email = obj.getEmail();
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
 	}
 	public String getName() {
 		return name;
--- src/main/java/com/yulken/workshopmongo/services/UserService.java
+++ src/main/java/com/yulken/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.yulken.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yulken.workshopmongo.domain.User;
 import com.yulken.workshopmongo.repository.UserRepository;
+import com.yulken.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,11 @@ public class UserService {
 		return repo.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (!user.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user.get();
+	}
 }
