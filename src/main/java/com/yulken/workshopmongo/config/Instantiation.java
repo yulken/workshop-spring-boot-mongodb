dae0f2d824508fe547d5db48a2b3c3e1a631f2bd
Caio Torres <caiocftorres@gmail.com>
2020-08-01 19:39:47 -0300

    Creating posts with nested users

--- src/main/java/com/yulken/workshopmongo/config/Instantiation.java
+++ src/main/java/com/yulken/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.yulken.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.yulken.workshopmongo.domain.Post;
 import com.yulken.workshopmongo.domain.User;
+import com.yulken.workshopmongo.repository.PostRepository;
 import com.yulken.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,26 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Post p1 = new Post(null, sdf.parse("2018-03-21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post p2 = new Post(null, sdf.parse("2018-03-23"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(p1,p2));
 	}
 	
 }
